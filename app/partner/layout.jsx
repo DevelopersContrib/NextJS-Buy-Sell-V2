bec6ab1513b27b049fe22462ec033a5048e7f719
Stephen <stephen.catacte@gmail.com>
2023-07-13 14:29:00 +0800

    meta data

--- app/partner/layout.jsx
+++ app/partner/layout.jsx
@@ -3,6 +3,16 @@ import Navigation from '../../components/Navigation';
 import Footer from '../../components/Footer';
 import { getDomain } from '../../lib/data';
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  const c = await getData();
+  return {
+    title: c.data.title ? c.data.title : 'Welcome to '+c.data.domainName,
+    description: c.data.description,
+    keywords: c.data.keywords,
+    author: c.data.author
+  }
+}
+
 export default function PartnerLayout({ children }) {
   const domain = getDomain()
   return (
