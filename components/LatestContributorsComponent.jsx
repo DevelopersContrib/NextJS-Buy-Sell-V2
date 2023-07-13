56847a030269eb7490eee5d686e9912bc86a7661
jaysoncano <cano.jayson10@gmail.com>
2023-07-13 13:15:35 +0800

    update contributor

--- components/LatestContributorsComponent.jsx
+++ components/LatestContributorsComponent.jsx
@@ -4,20 +4,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons'
 import LoadingState from "./LoadingState";
 
-const LatestContributorComponent = ({contributors}) => {
+const LatestContributorComponent = ({ contributors }) => {
   return (
     <div className="card text-bg-light mb-3">
       <div className="card-header">Latest Contributor</div>
-      {
-        !contributors ? <LoadingState />:(
-          contributors.data.map(c => (
-            <div className="card-body tw-overflow-y-auto tw-max-h-[350px]">
-              <ul className="list-unstyled">
-                <li>
-                  <div className="d-flex">
+      <div className="card-body tw-overflow-y-auto tw-max-h-[350px]">
+        <ul className="list-unstyled">
+          {
+            !contributors ? <LoadingState /> : (
+              contributors.data.map((c, index) => (
+                <li key={index}>
+                  <div className="d-flex mb-3">
                     <div className="flex-shrink-0">
                       <Image
-                        src={!c.profile_image ? 'https://contrib.com/img/timthumb.php?src=https://www.contrib.com/img/avatar0.jpg&w=115&h=115':'https://www.contrib.com/img/timthumb.php?src=uploads/profile/'+c.profile_image+'&w=115&h=115'}
+                        src={!c.profile_image ? 'https://contrib.com/img/timthumb.php?src=https://www.contrib.com/img/avatar0.jpg&w=115&h=115' : 'https://www.contrib.com/img/timthumb.php?src=uploads/profile/' + c.profile_image + '&w=115&h=115'}
                         width={65}
                         height={65}
                         alt=""
@@ -44,11 +44,11 @@ const LatestContributorComponent = ({contributors}) => {
                     </div>
                   </div>
                 </li>
-              </ul>
-            </div>
-          ))
-        )
-      }
+              ))
+            )
+          }
+        </ul>
+      </div>
     </div>
   )
 }
