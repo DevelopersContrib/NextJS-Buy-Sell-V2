291d077fd9a741671cdd0ad1f151051927124f10
Stephen <stephen.catacte@gmail.com>
2023-07-13 14:23:53 +0800

    meta data

--- app/buy/layout.jsx
+++ app/buy/layout.jsx
@@ -3,8 +3,19 @@ import Navigation from '../../components/Navigation';
 import Footer from '../../components/Footer';
 import { getDomain } from '../../lib/data';
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  const c = await getData();
+  return {
+    title: c.data.title ? c.data.title : 'Welcome to '+c.data.domainName,
+    description: c.data.description,
+    keywords: c.data.keywords,
+    author: c.data.author
+  }
+}
+
 export default function BuyLayout({ children }) {
   const domain = getDomain()
+
   return (
     <>
       <Navigation domain={domain} />
